c4b3332805ab8242640abe17800201e8c5de7771
Pedro Alisson <pedrodevelope@gmail.com>
2020-02-02 19:57:27 -0300

    Adjuste port

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', (req, resp) => {
     resp.render('home')
 })
 
-app.listen(3000, (err) =>{
+app.listen(8080, (err) =>{
     if(err){
         console.log("Não foi possivel rodar o servidor", err)
     }else{
