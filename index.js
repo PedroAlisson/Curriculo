61cb3debc0774650580603b1e7031e16ff4d4336
PedroAlisson <pedrodevelope@gmail.com>
2021-07-07 09:53:53 -0300

    Add Card imageAppFinancial and Link Api

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('/', (req, resp) => {
-    resp.render('home')
-})
+app.get("/", (req, resp) => {
+  resp.render("home");
+});
 
-app.listen(process.env.PORT || 3000, (err) =>{
-    if(err){
-        console.log("Não foi possivel rodar o servidor", err)
-    }else{
-        console.log("Servidor rodando")
-    }
-})
+app.listen(process.env.PORT || 3000, (err) => {
+  if (err) {
+    console.log("Não foi possivel rodar o servidor", err);
+  } else {
+    console.log("Servidor rodando");
+  }
+});
